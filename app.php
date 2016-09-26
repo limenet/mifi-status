b45bf164d50ab6b7667dbe7dd3aad307e1ba0c23
Linus Metzler <linus.metzler@gmail.com>
2016-09-26 11:34:43 +0200

    Applied fixes from StyleCI (#1)

--- app.php
+++ app.php
@@ -1,110 +1,107 @@
 <?php
+
 require 'vendor/autoload.php';
 
+use Curl\Curl;
 use Gui\Application;
 use Gui\Components\Label;
-use Gui\Output;
-use Curl\Curl;
-
 
 $application = new Application([
-    'title' => 'MiFi Status',
-    'width' => 384,
+    'title'  => 'MiFi Status',
+    'width'  => 384,
     'height' => 100,
-    'icon' => __DIR__.'/wifi.ico',
+    'icon'   => __DIR__.'/wifi.ico',
 ]);
 
-$application->on('start', function() use ($application) {
-
+$application->on('start', function () use ($application) {
     $application->setVerboseLevel(0);
 
     new Label([
-        'text' => "Connection",
-        'fontSize' => 10,
-        'top' => 5,
+        'text'       => 'Connection',
+        'fontSize'   => 10,
+        'top'        => 5,
         'fontFamily' => 'Source Code Pro',
-        'left' => 5,
+        'left'       => 5,
     ]);
     new Label([
-        'text' => "Network",
-        'fontSize' => 10,
-        'top' => 20,
+        'text'       => 'Network',
+        'fontSize'   => 10,
+        'top'        => 20,
         'fontFamily' => 'Source Code Pro',
-        'left' => 5,
+        'left'       => 5,
     ]);
     new Label([
-        'text' => "SIM",
-        'fontSize' => 10,
-        'top' => 35,
+        'text'       => 'SIM',
+        'fontSize'   => 10,
+        'top'        => 35,
         'fontFamily' => 'Source Code Pro',
-        'left' => 5,
+        'left'       => 5,
     ]);
     new Label([
-        'text' => "Usage",
-        'fontSize' => 10,
-        'top' => 50,
+        'text'       => 'Usage',
+        'fontSize'   => 10,
+        'top'        => 50,
         'fontFamily' => 'Source Code Pro',
-        'left' => 5,
+        'left'       => 5,
     ]);
     new Label([
-        'text' => "Battery",
-        'fontSize' => 10,
-        'top' => 65,
+        'text'       => 'Battery',
+        'fontSize'   => 10,
+        'top'        => 65,
         'fontFamily' => 'Source Code Pro',
-        'left' => 5,
+        'left'       => 5,
     ]);
     new Label([
-        'text' => "Speed (up/down)",
-        'fontSize' => 10,
-        'top' => 80,
+        'text'       => 'Speed (up/down)',
+        'fontSize'   => 10,
+        'top'        => 80,
         'fontFamily' => 'Source Code Pro',
-        'left' => 5,
+        'left'       => 5,
     ]);
 
     $connectionLabel = new Label([
-        'text' => "-",
-        'fontSize' => 10,
-        'top' => 5,
+        'text'       => '-',
+        'fontSize'   => 10,
+        'top'        => 5,
         'fontFamily' => 'Source Code Pro',
-        'left' => 144,
+        'left'       => 144,
     ]);
     $networkLabel = new Label([
-        'text' => "-",
-        'fontSize' => 10,
-        'top' => 20,
+        'text'       => '-',
+        'fontSize'   => 10,
+        'top'        => 20,
         'fontFamily' => 'Source Code Pro',
-        'left' => 144,
+        'left'       => 144,
     ]);
     $simLabel = new Label([
-        'text' => "-",
-        'fontSize' => 10,
-        'top' => 35,
+        'text'       => '-',
+        'fontSize'   => 10,
+        'top'        => 35,
         'fontFamily' => 'Source Code Pro',
-        'left' => 144,
+        'left'       => 144,
     ]);
     $usageLabel = new Label([
-        'text' => "-",
-        'fontSize' => 10,
-        'top' => 50,
+        'text'       => '-',
+        'fontSize'   => 10,
+        'top'        => 50,
         'fontFamily' => 'Source Code Pro',
-        'left' => 144,
+        'left'       => 144,
     ]);
     $batteryLabel = new Label([
-        'text' => "-",
-        'fontSize' => 10,
-        'top' => 65,
+        'text'       => '-',
+        'fontSize'   => 10,
+        'top'        => 65,
         'fontFamily' => 'Source Code Pro',
-        'left' => 144,
+        'left'       => 144,
     ]);
     $speedLabel = new Label([
-        'text' => "-",
-        'fontSize' => 10,
-        'top' => 80,
+        'text'       => '-',
+        'fontSize'   => 10,
+        'top'        => 80,
         'fontFamily' => 'Source Code Pro',
-        'left' => 144,
+        'left'       => 144,
     ]);
-    $application->loop->addPeriodicTimer(5, function() use($connectionLabel, $networkLabel, $simLabel, $usageLabel, $batteryLabel, $speedLabel) {
-
+    $application->loop->addPeriodicTimer(5, function () use ($connectionLabel, $networkLabel, $simLabel, $usageLabel, $batteryLabel,$speedLabel) {
         $curl = new Curl();
         $curl->setTimeout(1);
         $curl->post('http://192.168.0.1/cgi-bin/qcmap_web_cgi', json_encode([
@@ -114,9 +111,9 @@ $application->on('start', function() use ($application) {
 
 
         $status = [
-            'connection' => ['disable','disconnected','connecting','disconnecting','connected'],
-            'network' => ['no service','GSM','WCDMA','LTE','TD-SCDMA','CDMA 1x','CDMA EVDO'],
-            'sim' => ['invalid','no SIM','error','ready','PIN requested','PIN verified','PUK requested','permanently locked'],
+            'connection' => ['disable', 'disconnected', 'connecting', 'disconnecting', 'connected'],
+            'network'    => ['no service', 'GSM', 'WCDMA', 'LTE', 'TD-SCDMA', 'CDMA 1x', 'CDMA EVDO'],
+            'sim'        => ['invalid', 'no SIM', 'error', 'ready', 'PIN requested', 'PIN verified', 'PUK requested', 'permanently locked'],
         ];
 
         $data = json_decode($curl->response);
