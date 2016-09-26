abf27d1a2fc21c9e09db9d67893cce5e0eb6886f
Linus Metzler <linus.metzler@gmail.com>
2016-09-26 21:58:36 +0200

    added desktop application

---
+++ desktop/mifi-status/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace mifi_status
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private MainWindow mainWindow;
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            this.mainWindow = new MainWindow();
+            mainWindow.Show();
+            mainWindow.refreshData();
+
+            var dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            dispatcherTimer.Start();
+            dispatcherTimer.Tick += (sd, ea) => { HandleTimer(sender, e); };
+        }
+
+        private void HandleTimer(object sender, EventArgs e)
+        {
+            mainWindow.refreshData();
+        }
+    }
+}
---
+++ desktop/mifi-status/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Net;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.IO;
+
+namespace mifi_status
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void refreshData()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://192.168.0.1/cgi-bin/qcmap_web_cgi");
+            request.ServicePoint.Expect100Continue = false;
+            request.Method = "POST";
+            byte[] data = Encoding.UTF8.GetBytes("{\"module\":\"status\",\"action\":0}");
+            request.ContentLength = data.Length;
+            Stream stream = request.GetRequestStream();
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+
+            dynamic json = JsonConvert.DeserializeObject(reader.ReadToEnd());
+
+            string[] connections = { "disable", "disconnected", "connecting", "disconnecting", "connected" };
+            string[] networks = { "no service", "GSM", "WCDMA", "LTE", "TD-SCDMA", "CDMA 1x", "CDMA EVDO" };
+            string[] sims = { "invalid", "no SIM", "error", "ready", "PIN requested", "PIN verified", "PUK requested", "permanently locked" };
+
+            this.statusConn.Content = connections[json.wan.connectStatus];
+            this.statusBatt.Content = json.battery.voltage + "%";
+            this.statusNet.Content = networks[json.wan.networkType];
+            this.statusSIM.Content = sims[json.wan.simStatus];
+            this.statusUse.Content = getFilesizeHuman(Convert.ToDouble(json.wan.totalStatistics));
+            this.statusSpeed.Content = getFilesizeHuman(Convert.ToDouble(json.wan.txSpeed)) + " / " + getFilesizeHuman(Convert.ToDouble(json.wan.rxSpeed));
+
+        }
+
+        public string getFilesizeHuman(double len)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB" };
+            int order = 0;
+            while (len >= 1024 && ++order < sizes.Length)
+            {
+                len = len / 1024;
+            }
+
+            // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+            // show a single decimal place, and no space.
+            return String.Format("{0:0.##} {1}", len, sizes[order]);
+        }
+    }
+}
